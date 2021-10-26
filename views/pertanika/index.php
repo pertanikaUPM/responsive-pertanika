5b0fab80cb7cbaccda2d88b8478f2b5d5db3c586
pertanikaUPM <pertanika_it@upm.edu.my>
2021-10-26 16:21:37 +0800

    add style.css

--- views/pertanika/index.php
+++ views/pertanika/index.php
@@ -2,113 +2,113 @@
 <html lang="en">
 
 <head>
-    <title>Bootstrap Example</title>
+    <title>Pertanika Journals</title>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+    <link rel="stylesheet" href="/lib/style.css">
     <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
     <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
 </head>
 
-<body>
-    <div class="text-center">
-        <img src="resources/img/upper-banner.webp" class="img-fluid" alt="logo-banner">
-    </div>
-    <header>
-        <nav class="navbar navbar-expand-lg navbar-dark px-5" style="background-color: #c9234b;">
-            <!-- <a class="navbar-brand" href="#">Navbar</a> -->
-            <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarTogglerDemo02" aria-controls="navbarTogglerDemo02" aria-expanded="false" aria-label="Toggle navigation">
-                <span class="navbar-toggler-icon"></span>
-            </button>
-
-            <div class="collapse navbar-collapse" id="navbarTogglerDemo02">
-                <ul class="navbar-nav mr-auto mt-2 mt-lg-0">
-                    <li class="nav-item active">
-                        <a class="nav-link" href="/">Home <span class="sr-only">(current)</span></a>
-                    </li>
-                    <li class="nav-item">
-                        <a class="nav-link" href="#">Introduction</a>
-                    </li>
-                    <li class="nav-item">
-                        <a class="nav-link" href="#">Journal Division</a>
-                    </li>
-                    <li class="nav-item">
-                        <a class="nav-link" href="#">Staff Info</a>
-                    </li>
-                    <li class="nav-item dropdown">
-                        <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                            Journals
-                        </a>
-                        <div class="dropdown-menu" aria-labelledby="navbarDropdown">
-                            <a class="dropdown-item" href="#">Journal of Tropical Agricultural Science</a>
-                            <a class="dropdown-item" href="#">Journal of Science & Technology</a>
-                            <a class="dropdown-item" href="#">Journal of Social Sciences & Humanities</a>
-                        </div>
-                    </li>
-                    <li class="nav-item">
-                        <a class="nav-link" href="#">Contact</a>
-                    </li>
-                    <li class="nav-item">
-                        <a class="nav-link" href="#">UPM Press</a>
-                    </li>
-                    <li class="nav-item">
-                        <a class="nav-link" href="https://www.facebook.com/pertanika/">Find Us On <i class="fa fa-facebook-square" width="30" height="30" class="d-inline-block align-top"></i></a>
-                    </li>
-                </ul>
-            </div>
-        </nav>
-    </header>
-    <div id="carousel" class="carousel slide" data-ride="carousel">
-        <ol class="carousel-indicators">
-            <li data-target="#carouselExampleIndicators" data-slide-to="0" class="active"></li>
-            <li data-target="#carouselExampleIndicators" data-slide-to="1"></li>
-            <li data-target="#carouselExampleIndicators" data-slide-to="2"></li>
-        </ol>
-        <div class="carousel-inner">
-            <div class="carousel-item active">
-                <img class="d-block w-100" src="resources/img/slideshows/1.png" alt="First slide">
-            </div>
-            <div class="carousel-item">
-                <img class="d-block w-100" src="resources/img/slideshows/2.png" alt="Second slide">
-            </div>
-            <div class="carousel-item">
-                <img class="d-block w-100" src="resources/img/slideshows/3.png" alt="Third slide">
-            </div>
+<body style="background: #eeeeee !important;">
+    <div class="container" style="background: #ffffff !important;">
+        <div class="text-center">
+            <img src="resources/img/upper-banner.webp" class="img-fluid" alt="logo-banner">
         </div>
-        <a class="carousel-control-prev" href="#carouselExampleIndicators" role="button" data-slide="prev">
-            <span class="carousel-control-prev-icon" aria-hidden="true"></span>
-            <span class="sr-only">Previous</span>
-        </a>
-        <a class="carousel-control-next" href="#carouselExampleIndicators" role="button" data-slide="next">
-            <span class="carousel-control-next-icon" aria-hidden="true"></span>
-            <span class="sr-only">Next</span>
-        </a>
-    </div>
+        <header>
+            <nav class="navbar navbar-expand-lg navbar-dark px-5" style="background-color: #c9234b;">
+                <!-- <a class="navbar-brand" href="#">Navbar</a> -->
+                <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarTogglerDemo02" aria-controls="navbarTogglerDemo02" aria-expanded="false" aria-label="Toggle navigation">
+                    <span class="navbar-toggler-icon"></span>
+                </button>
 
-    <div class="container">
+                <div class="collapse navbar-collapse" id="navbarTogglerDemo02">
+                    <ul class="navbar-nav mr-auto mt-2 mt-lg-0">
+                        <li class="nav-item active">
+                            <a class="nav-link" href="/">Home <span class="sr-only">(current)</span></a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="#">Introduction</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="#">Journal Division</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="#">Staff Info</a>
+                        </li>
+                        <li class="nav-item dropdown">
+                            <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                                Journals
+                            </a>
+                            <div class="dropdown-menu" aria-labelledby="navbarDropdown">
+                                <a class="dropdown-item" href="#">Journal of Tropical Agricultural Science</a>
+                                <a class="dropdown-item" href="#">Journal of Science & Technology</a>
+                                <a class="dropdown-item" href="#">Journal of Social Sciences & Humanities</a>
+                            </div>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="#">Contact</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="#">UPM Press</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="https://www.facebook.com/pertanika/">Find Us On <i class="fa fa-facebook-square" width="30" height="30" class="d-inline-block align-top"></i></a>
+                        </li>
+                    </ul>
+                </div>
+            </nav>
+        </header>
+        <div id="carousel" class="carousel slide" data-ride="carousel">
+            <ol class="carousel-indicators">
+                <li data-target="#carouselExampleIndicators" data-slide-to="0" class="active"></li>
+                <li data-target="#carouselExampleIndicators" data-slide-to="1"></li>
+                <li data-target="#carouselExampleIndicators" data-slide-to="2"></li>
+            </ol>
+            <div class="carousel-inner">
+                <div class="carousel-item active">
+                    <img class="d-block w-100" src="resources/img/slideshows/1.png" alt="First slide">
+                </div>
+                <div class="carousel-item">
+                    <img class="d-block w-100" src="resources/img/slideshows/2.png" alt="Second slide">
+                </div>
+                <div class="carousel-item">
+                    <img class="d-block w-100" src="resources/img/slideshows/3.png" alt="Third slide">
+                </div>
+            </div>
+            <a class="carousel-control-prev" href="#carouselExampleIndicators" role="button" data-slide="prev">
+                <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+                <span class="sr-only">Previous</span>
+            </a>
+            <a class="carousel-control-next" href="#carouselExampleIndicators" role="button" data-slide="next">
+                <span class="carousel-control-next-icon" aria-hidden="true"></span>
+                <span class="sr-only">Next</span>
+            </a>
+        </div>
         <div class="row my-3">
             <div class="col-sm-4">
-                <a class="link-container jtas-bg" href="/pjtas">
-                    <img src="resources/img/jtas.webp" alt="pjtas-journal-cover" class="image" style="width:100%; height: 415px;">
-                    <div class="middle">
-                        <div class="text">Go to Journal of Tropical Agricultural Science</div>
+                <a class="journal-cover-container jtas-bg" href="/pjtas">
+                    <img src="resources/img/jtas.webp" alt="pjtas-journal-cover" class="journal-cover-image" style="width:100%; height: 415px;">
+                    <div class="journal-cover-body">
+                        <div class="journal-cover-text">Go to Journal of Tropical Agricultural Science</div>
                     </div>
                 </a>
             </div>
             <div class="col-sm-4">
-                <a class="link-container jst-bg" href="/pjst">
-                    <img src="resources/img/jst.webp" alt="pjst-journal-cover" class="image" style="width:100%; height: 415px;">
-                    <div class="middle">
-                        <div class="text">Go to Journal of Science & Technology</div>
+                <a class="journal-cover-container jst-bg" href="/pjst">
+                    <img src="resources/img/jst.webp" alt="pjst-journal-cover" class="journal-cover-image" style="width:100%; height: 415px;">
+                    <div class="journal-cover-body">
+                        <div class="journal-cover-text">Go to Journal of Science & Technology</div>
                     </div>
                 </a>
             </div>
             <div class="col-sm-4">
-                <a class="link-container jssh-bg" href="/pjssh">
-                    <img src="resources/img/jssh.webp" alt="pjssh-journal-cover" class="image" style="width:100%; height: 415px;">
-                    <div class="middle">
-                        <div class="text">Go to Journal of Social Sciences & Humanities</div>
+                <a class="journal-cover-container jssh-bg" href="/pjssh">
+                    <img src="resources/img/jssh.webp" alt="pjssh-journal-cover" class="journal-cover-image" style="width:100%; height: 415px;">
+                    <div class="journal-cover-body">
+                        <div class="journal-cover-text">Go to Journal of Social Sciences & Humanities</div>
                     </div>
                 </a>
             </div>
